3cb0b88816dcf423368b069c7296e45afc5492b2
dougiefresh1233 <douglas.cannon18@gmail.com>
2016-10-29 15:55:55 -0500

    Added a tsting activity

---
+++ Grocery_Finder/app/src/main/java/com/example/dougl/grocery_finder/Test_API_Activity.java
@@ -0,0 +1,13 @@
+package com.example.dougl.grocery_finder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Test_API_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_api);
+    }
+}
